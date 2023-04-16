b85983514baaefeb689139cb2d7fbab1960ba896
E Fike <fikeem@g.cofc.edu>
2023-04-16 19:45:27 -0400

    update, assuming copy from a local server

--- testapp01/cmd/Copy_to_share.cmd
+++ testapp01/cmd/Copy_to_share.cmd
@@ -1,7 +1,7 @@
 REM script to copy file to share
 set myErrors=0
 
-copy C:\path_to_directory\testapp01\output\test.txt \\washdc.test.lab\rm15fs01\crms\
+copy \\localserver\path_to_directory\testapp01\output\test.txt \\washdc.test.lab\rm15fs01\crms\
 if %errorlevel% NEQ 0 set /A myErrors+=1
 
 :end
