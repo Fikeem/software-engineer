619ded2fa1ad3b1a7bda0988c26cc3e03c97e263
E Fike <fikeem@g.cofc.edu>
2023-04-16 14:15:26 -0400

    Added some notes..

--- testdb01/cmd/db_login.cmd
+++ testdb01/cmd/db_login.cmd
@@ -3,9 +3,15 @@ REM script to set passwords and connectstrings to login to db
 REM accounts and passwords in this file are not real and are only for demonstration and case study purposes. in a real production environment passwords are encrypted and retrieved from a secure secrets store.
 
 set DB_SID=GFMP
+REM Oracle DB - SystemID: GFMP?
+
 
 @echo off
+REM turns off command echoing in the batch script, so commands are not displayed in the console as they are executed.
 
+REM %1 == first command-line arg
+
+REM arg == opstest
 if /i "%1" EQU "opstest" (
 set USER_NAME=%~1
 set PASS_WORD=strongPass1
@@ -27,6 +33,8 @@ goto connectstring
 echo %~n0: %~1 is an invalid user
 goto abort_end
 
+
+REM Jump to connection after IF, ELSE Fails
 :connectstring
 set CONNECTSTRING=%USER_NAME%/%PASS_WORD%@%DB_SID%
 
