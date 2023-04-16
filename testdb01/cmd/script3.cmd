d90450e4ff8ad1eb9fbfbc7e3e72857f36e77dae
E Fike <fikeem@g.cofc.edu>
2023-04-16 17:33:11 -0400

    update script3 check for input files

--- testdb01/cmd/script3.cmd
+++ testdb01/cmd/script3.cmd
@@ -1,6 +1,19 @@
 REM script3.cmd - script to create csv
 set myErrors=0
 
+REM check for input files
+if not exist \testapp01\input\sql1.txt (
+    echo sql1.txt not found
+    set /A myErrors+=1
+)
+if not exist \testapp01\input\sql2.txt (
+    echo sql2.txt not found
+    set /A myErrors+=1
+)
+
+REM check if any input files are missing and terminate the script if so
+if %myErrors% NEQ 0 goto badend
+
 REM use input files to create output
 for /F "tokens=* delims=*" %%G in (\testapp01\input\sql1.txt) do (
 @echo %%G,>>\testapp01\output\analysis.csv
@@ -16,5 +29,5 @@ exit /b %myErrors%
 
 :badend
 set myErrors=1
-echo script failed
+echo script failed, one or more input files not found
 exit /b %myErrors%
