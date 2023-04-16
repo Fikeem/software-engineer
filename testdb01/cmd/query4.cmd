2d995113142a9c2a60668f2ea1bfe8d4ff8eaf0e
E Fike <fikeem@g.cofc.edu>
2023-04-16 17:32:03 -0400

    update query 4 error checking

--- testdb01/cmd/query4.cmd
+++ testdb01/cmd/query4.cmd
@@ -1,8 +1,6 @@
 REM query4.cmd - script to query the db and copy output
 set myErrors=0
 
-
-
 REM get password
 d:
 call \testdb01\cmd\db_login.cmd opstest
@@ -11,16 +9,24 @@ if %errorlevel% NEQ 0 goto badend
 @echo on
 REM run sql
 sqlplus -l %connectstring% @\testdb01\sql\query4.sql
-if %errorlevel% NEQ 0 (set /A myErrors+=1)
+if %errorlevel% NEQ 0 (
+set msg="could not run query"
+goto badend
+)
 
 REM copy output
 copy \testdb01\log\sql4.log \testapp01\input\
-if %errorlevel% NEQ 0 (set /A myErrors+=1)
+if %errorlevel% NEQ 0 (
+set msg="could not copy file"
+goto badend
+)
 
 :end
+set myErrors=0
+echo Success!
 exit /b %myErrors%
 
 :badend
 set myErrors=1
-echo script failed
+echo script failed %msg%
 exit /b %myErrors%
